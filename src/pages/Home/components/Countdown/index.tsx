ba1c0cc5ad082b9189f9e4bf5297f7570bd504cd
EsleySaab <esleysantana.sa@gmail.com>
2024-12-28 19:28:49 -0300

    feat: creating and sharing context between routes

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,12 +1,16 @@
 import { differenceInSeconds } from "date-fns"
-import { useState, useEffect, useContext } from "react"
+import { useEffect, useContext } from "react"
 import { CountdownContainer, Separator } from "./styles"
-import { CyclesContext } from "../.."
+import { CyclesContext } from "../../../../contexts/CyclesContext"
 
 export function Countdown() {
-  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } =
-    useContext(CyclesContext)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+  const {
+    activeCycle,
+    activeCycleId,
+    markCurrentCycleAsFinished,
+    amountSecondsPassed,
+    setSecondsPassed,
+  } = useContext(CyclesContext)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
 
@@ -23,10 +27,10 @@ export function Countdown() {
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
 
-          setAmountSecondsPassed(totalSeconds)
+          setSecondsPassed(totalSeconds)
           clearInterval(interval)
         } else {
-          setAmountSecondsPassed(secondsDifference)
+          setSecondsPassed(secondsDifference)
         }
       }, 1000)
 
@@ -34,7 +38,13 @@ export function Countdown() {
         clearInterval(interval)
       }
     }
-  }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished])
+  }, [
+    activeCycle,
+    totalSeconds,
+    activeCycleId,
+    setSecondsPassed,
+    markCurrentCycleAsFinished,
+  ])
 
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
